376cf4a61f1aca09b1d4e5fbc38bb0750eede5cf
Bening Kumala Haqi <bkhaqi@gmail.com>
2023-05-08 17:57:32 +0700

    use postgres as database

--- configs/database.go
+++ configs/database.go
@@ -1,22 +1,23 @@
 package configs
 
 import (
-	"task5-vix/models"
 	"log"
+	"task5-vix/models"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDB() {
-	db, err := gorm.Open(mysql.Open("root:root@tcp(127.0.0.1:3307)/test?parseTime=true"), &gorm.Config{})
+	dsn := "host=localhost user=postgres password=postgres dbname=vix_db port=5432 sslmode=disable TimeZone=Asia/Singapore"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	db.AutoMigrate(&models.User{}) 
+	db.AutoMigrate(&models.User{})
 	db.AutoMigrate(&models.Photo{})
 
 	DB = db
